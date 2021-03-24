eb7b589b2c0d04fe11f550a6145803ee5bb0ff35
giu0213@naver.com <giu0213@naver.com>
2021-03-24 14:40:57 +0900

    특정 타이밍 스크롤 애니메이션 적용(아직 메세지 한개만 적용함)

--- js/basic.js
+++ js/basic.js
@@ -3,6 +3,7 @@
     let yOffset = 0; //스크롤 시 현재 위치 저장 변수
     let prevScrollHeight = 0 //현재 yOffset보다 이전에 위치한 섹션 scrollHeight
     let currentScene = 0 //현재 활성화된 scroll-section
+    let changeScene = false; //scene 변화 여부를 저장하는 변수
 
     const sceneInfo = [
         {
@@ -18,7 +19,12 @@
                 messageD: document.querySelector('#scroll-section-0 .main-message.d')
             },
             values: {
-                messageA_opacity: [0, 1]
+                messageA_opacity_in: [0, 1, { start: 0.1, end: 0.2 }],
+                messageA_opacity_out: [1, 0, { start: 0.25, end: 0.3}],
+                messageA_translateY_in: [20, 0, { start: 0.1, end: 0.2}],
+                messageA_translateY_out: [0, -20, { start: 0.25, end: 0.3}],
+
+                messageB_opacity: [0, 1, { start: 0.3, end: 0.4 }]
             } 
         },
         {
@@ -70,9 +76,59 @@
         document.body.setAttribute('id', `show-scene-${currentScene}`);
     }
 
+    function calcValues(values, currentYOffset) {
+        let result;
+        const scrollHeight = sceneInfo[currentScene].scrollHeight;
+        const scrollRatio = currentYOffset / scrollHeight;
+
+        if(values.length === 3) {
+            // start ~ end 사이에 애니메이션 실행 (부분 영역 애니메이션)
+            const partScrollStart = values[2].start * scrollHeight;
+            const partScrollEnd = values[2].end * scrollHeight;
+            const partScrollHeight = partScrollEnd - partScrollStart;
+
+            if(currentYOffset >= partScrollStart && currentYOffset <=partScrollEnd){
+                result = (currentYOffset - partScrollStart) / partScrollHeight * (values[1] - values[0]) + values[0];
+            }
+            else if(currentYOffset < partScrollStart){ result = values[0]; }
+            else if(currentYOffset > partScrollEnd) { result = values[1]; }
+            
+
+        } 
+        else {
+            //현재 씬 전체에서의 애니메이션
+            result = scrollRatio * (values[1] - values[0]) + values[0];   
+        }
+        
+        return result;
+    }
+
     function playAnimation() {
+        const objs = sceneInfo[currentScene].objs;
+        const values = sceneInfo[currentScene].values;
+        const currentYOffset = yOffset - prevScrollHeight;
+        const scrollHeight = sceneInfo[currentScene].scrollHeight;
+        const scrollRatio = currentYOffset / scrollHeight;
+
         switch (currentScene) {
             case 0:
+                let fadeIn_a = calcValues(values.messageA_opacity_in, currentYOffset);
+                let fadeOut_a = calcValues(values.messageA_opacity_out, currentYOffset);
+                let translateY_in_a = calcValues(values.messageA_translateY_in, currentYOffset);
+                let translateY_out_a = calcValues(values.messageA_translateY_out, currentYOffset);
+
+                let point = (values.messageA_opacity_out[2].start + values.messageA_opacity_in[2].end) / 2
+
+                if( scrollRatio <= point){
+                    objs.messageA.style.opacity = fadeIn_a;
+                    objs.messageA.style.transform = `translateY(${translateY_in_a}%)`;
+                }
+                else {
+                    objs.messageA.style.opacity = fadeOut_a;
+                    objs.messageA.style.transform = `translateY(${translateY_out_a}%)`;
+                }
+
+
                 break;
             case 1:
                 break;
@@ -84,21 +140,27 @@
     }
     
     function scrollLoop() {
+        changeScene = false;
         prevScrollHeight = 0;
+
         for(let i = 0; i < currentScene ; i++) {
             prevScrollHeight += sceneInfo[i].scrollHeight;
         }
 
         if(yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+            changeScene = true;
             currentScene++;
             document.body.setAttribute('id', `show-scene-${currentScene}`);
         }
 
         if(yOffset < prevScrollHeight) {
             if(currentScene === 0) return;
+            changeScene = true;
             currentScene--;
             document.body.setAttribute('id', `show-scene-${currentScene}`);
         }
+
+        if(changeScene) return; //씬이 바뀔 때 이상한 값이 들어가는 것 방지
 
         playAnimation();
     }
